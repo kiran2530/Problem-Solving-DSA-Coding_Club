d62917375f376bc35e94fcc508e70ada5316096d
kirannikam <nikamsarkar464@gmail.com>
2023-09-24 21:29:34 +0530

    MaxProfit.java added

---
+++ MaxProfit.java
@@ -0,0 +1,17 @@
+public class MaxProfit {
+    public static int maxProfit(int[] a) {
+        int max = 0;
+        for(int i=0; i<a.length-1; i++)  {
+            for(int j=i+1; j<a.length; j++) {
+                if(a[j]-a[i] > max) 
+                    max = a[j]-a[i];
+            }
+        }
+        return max;
+    }
+
+    public static void main(String[] args) {
+        int a[] = {7,1,5,3,6,4};
+        System.out.println(maxProfit(a));
+    }
+}
