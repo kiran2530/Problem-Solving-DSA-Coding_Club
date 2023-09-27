419d9e3880c595977a2fa2e23dec907b63436599
kirannikam <nikamsarkar464@gmail.com>
2023-09-27 23:10:19 +0530

    NotDistinct.java and .class file added

---
+++ NotDistinct.java
@@ -0,0 +1,21 @@
+import java.util.ArrayList;
+
+public class NotDistinct {
+    public static boolean isNotDistinct(int[] a) {
+        ArrayList<Integer> list = new ArrayList<>();
+
+        for(int i = 0; i<a.length; i++) {
+            if(list.contains(a[i])) {
+                return true;
+            }
+            else{
+                list.add(a[i]);
+            }
+        }
+        return false;
+    }
+    public static void main(String[] args) {
+        int[] nums = {1,2,3,1};
+        System.out.println(isNotDistinct(nums));
+    }
+}
