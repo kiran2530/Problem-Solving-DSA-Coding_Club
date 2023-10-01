0d791cd6ff019450c23aaf85e1b0b43fcdb2152d
kirannikam <nikamsarkar464@gmail.com>
2023-10-01 23:32:08 +0530

    flowerbed.java and flowerbed.class added

--- Flowerbed.java
+++ Flowerbed.java
@@ -1,30 +1,27 @@
 import java.util.ArrayList;
 public class Flowerbed {
     public static boolean isPlanted(ArrayList<Integer> a, int n) {
-        for(int i = 0; i< a.size()-1; i++) {
-            if(a.get(i) == 1)
-                continue;
-            else if(a.get(i+1) == 1 || a.get(i-1) == 1)
-                continue;
-            else {
-                n--;
+        for(int i = 0; i< a.size(); i++) {
+            if(a.get(i)==0 && (i == 0 || a.get(i-1) == 0) && (i == a.size()-1 || a.get(i+1) == 0)) {
                 a.set(i, 1);
-                if(n == 0){
+                n--;
+                if(n == 0) 
                     return true;
-                }
             }
         }
         return false;
     }
     public static void main(String[] args) {
         ArrayList<Integer> a = new ArrayList<>();
+        a.add(0);
+        a.add(0);
+        a.add(0);
+        a.add(0);
         a.add(1);
         a.add(0);
         a.add(0);
-        a.add(0);
-        a.add(1);
         
-        int n = 2;
+        int n = 3;
 
         System.out.println(isPlanted(a, n));
     }
