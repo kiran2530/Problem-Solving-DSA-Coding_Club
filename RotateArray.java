d787405a1077b367fb4d9cfcb549c8ace0e31981
kirannikam <nikamsarkar464@gmail.com>
2023-10-05 23:58:01 +0530

    RotateArray.java updated.

--- RotateArray.java
+++ RotateArray.java
@@ -1,10 +1,10 @@
 import java.util.ArrayList;
 public class RotateArray {
     public static ArrayList<Integer> rotateArray(ArrayList<Integer> a, int k) {
-        if(k == 0)
+        if(k % a.size() == 0)
             return a;
-        if(k > a.size())
-            return a;
+        else
+            k = (k % a.size());
         
         int x = a.size()-1;
         for(int i = 0; i<k; i++) {
@@ -19,11 +19,11 @@ public class RotateArray {
         a.add(1);
         a.add(2);
         a.add(3);
-        a.add(4);
-        a.add(5);
-        a.add(6);
-        a.add(7);
-        int k = 3;
+        // a.add(4);
+        // a.add(5);
+        // a.add(6);
+        // a.add(7);
+        int k = 2;
 
         System.out.println(rotateArray(a, k));
     }
