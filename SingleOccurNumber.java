e1b71202f2cc238bd3e764cf64662bcc302dcc17
kirannikam <nikamsarkar464@gmail.com>
2023-12-27 11:29:07 +0530

    new files added.

---
+++ SingleOccurNumber.java
@@ -0,0 +1,17 @@
+import java.util.Arrays;
+
+public class SingleOccurNumber {
+    public static int findSingleOccurNumber(int[] arr, int n) {
+        Arrays.sort(arr);
+        for(int i=1; i<n; i+=2) {
+            if(arr[i] != arr[i-1])
+            return arr[i-1];
+        }
+        return arr[n-1];
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {2,3,5,4,5,3,4};
+        System.out.println(findSingleOccurNumber(arr, arr.length));
+    }
+}
