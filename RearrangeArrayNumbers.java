b16914c876ef09c05b5d315f202cf27b5dd175a1
kirannikam <nikamsarkar464@gmail.com>
2023-12-29 12:24:34 +0530

    Add Aproch 2

--- RearrangeArrayNumbers.java
+++ RearrangeArrayNumbers.java
@@ -3,6 +3,8 @@
        array. Rearrange the array such that A[i] = i and if i is not present, display -1 at that place. */
 
 import java.util.HashMap;
+// Aproch 1
+/*
 public class RearrangeArrayNumbers {
     public static int[] rearrangeArrayNumbers(int[] a, int n) {
         HashMap<Integer, Integer> map = new HashMap<>();
@@ -17,6 +19,36 @@ public class RearrangeArrayNumbers {
                 a[i] = -1;
         }
         return a;
+    }
+    public static void main(String[] args) {
+        int[] a = {-1,-1,6,1,9,3,2,-1,4,-1};
+        int[] rearrangedArr = rearrangeArrayNumbers(a, a.length);
+
+        for(int i=0; i<a.length; i++) {
+            System.out.print(rearrangedArr[i] + " ");
+        }
+    }
+}
+*/
+
+// Aproch 2
+public class RearrangeArrayNumbers {
+    public static int[] rearrangeArrayNumbers(int[] arr, int n) {
+        for(int i=0; i<n; i++) {
+            if(arr[i] != -1 && arr[i] != i) {
+                int a = arr[i];
+                if(arr[a] != -1 && arr[a] != a) {
+                    int b = arr[a];
+                    arr[a] = a;
+                    a = b;
+                }
+                arr[a] = a;
+                
+                if(arr[i] != i)
+                    arr[i] = -1;
+            }
+        }
+        return arr;
     }
     public static void main(String[] args) {
         int[] a = {-1,-1,6,1,9,3,2,-1,4,-1};
